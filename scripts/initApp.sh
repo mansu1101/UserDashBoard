8f3a76177bac723d13f36d767134d29206b58c42
MJ <manish.joshi@intimetec.com>
2019-01-19 00:11:32 +0530

    Modified initApp file

--- scripts/initApp.sh
+++ scripts/initApp.sh
@@ -3,7 +3,7 @@
 # Creates a database and register an Admin user.
 mongo data_store_point --eval 'db.user.insert({
 "username" : "Manish",
-"mobile": 9672281491
+"mobile": 9672281491,
 "email" : "manish.joshi89@gmail.com",
 "password" : "$2b$10$XvhQsSUr.cV8Wn5NPS7SSOHben1RTsrkWHvLL0eBXbg.gf5gmInl2",
 "role" : "Admin",
